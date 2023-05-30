8e5ea931c4d98b7591a71ba29f0fc41c4e2b38ed
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-05-30 14:18:41 +0200

    feat(server): Basic usage

--- server/src/main.c
+++ server/src/main.c
@@ -7,8 +7,21 @@
 
 #include <stdio.h>
 
+static void display_usage(void)
+{
+    printf("USAGE: ./zappy_server -p port -x width -y height -n name1 name2 ..."
+    " -c clientsNb -f freq\n"
+    "port\tis the port number\n"
+    "width\tis the width of the world\n"
+    "height\tis the height of the world\n"
+    "nameX\tis the name of the team X\n"
+    "clientsNb\tis the number of authorized clients per team\n"
+    "freq\tis the reciprocal of time unit for execution of actions"
+    );
+}
+
 int main(void)
 {
-    printf("Hello World!\n");
+    display_usage();
     return 0;
 }
