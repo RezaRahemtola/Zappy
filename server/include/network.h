3553043bd42133596264539f1ab808c39737bdf2
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-04 19:34:07 +0200

    feat(server/network): Socket initialization

---
+++ server/include/network.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** zappy
+** File description:
+** network
+*/
+
+#pragma once
+
+#include <netinet/in.h>
+
+#define NETWORK_MAX_CLIENTS 100
+
+typedef struct sockaddr_in sockaddr_in_t;
+typedef struct sockaddr sockaddr_t;
+
+typedef struct socket_s {
+    int fd;
+    sockaddr_in_t address;
+    socklen_t len;
+} socket_t;
