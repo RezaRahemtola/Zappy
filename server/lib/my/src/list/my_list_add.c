8e7c0d91d346b6eae83947920de7f339545c4b51
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-04 22:59:09 +0200

    feat(server): List lib

---
+++ server/lib/my/src/list/my_list_add.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2022
+** libmy
+** File description:
+** List adding functions
+*/
+
+#include <stdlib.h>
+#include "my_list.h"
+
+static list_t *init_node(void *data)
+{
+    list_t *node = malloc(sizeof(list_t));
+
+    if (node == NULL)
+        return (NULL);
+    node->data = data;
+    node->prev = NULL;
+    node->next = NULL;
+    return (node);
+}
+
+void list_add(list_t **list, void *data)
+{
+    list_t *node = init_node(data);
+    list_t *current = *list;
+
+    if (node == NULL)
+        return;
+    if (*list == NULL) {
+        *list = node;
+        return;
+    }
+    while (current->next != NULL)
+        current = current->next;
+    current->next = node;
+    node->prev = current;
+}
