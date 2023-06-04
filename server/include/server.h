2a81e9ff7c86140025df58ef2c886e24229242e9
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-04 23:16:31 +0200

    feat(server): Usage of new structure
    Server and client dedicated structures

--- server/include/server.h
+++ server/include/server.h
@@ -8,5 +8,16 @@
 #pragma once
 
 #include "parameters.h"
+#include "network.h"
+#include "my_list.h"
+
+typedef struct client_s {
+    socket_t socket;
+} client_t;
+
+typedef struct server_s {
+    socket_t *socket;
+    list_t *clients;
+} server_t;
 
 void launch_server(params_t *params);
