d355236e465b5f2bd7593609906ae7d51e086700
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-04 22:59:09 +0200

    feat(server): List lib

---
+++ server/lib/my/src/list/my_list_remove.c
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2022
+** libmy
+** File description:
+** List removing functions
+*/
+
+#include <stdlib.h>
+#include "my_list.h"
+
+static void remove_data(list_t *node, list_t **list, free_func free_func)
+{
+    list_t *prev = node->prev;
+    list_t *next = node->next;
+
+    if (prev == NULL)
+        *list = next;
+    else
+        prev->next = next;
+    if (next != NULL)
+        next->prev = prev;
+    if (free_func != NULL)
+        free_func(node->data);
+    free(node);
+}
+
+void list_remove(list_t **list, void *data, equals_func eq_func,
+free_func free_func)
+{
+    list_t *current = *list;
+
+    while (current != NULL) {
+        if (eq_func(current->data, data)) {
+            remove_data(current, list, free_func);
+            return;
+        }
+        current = current->next;
+    }
+}
