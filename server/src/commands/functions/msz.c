843a7ffca91a9fcfd1bb2b110da1519b82bbe9dd
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-09 01:39:09 +0200

    feat(server): msz command implemented

--- server/src/commands/functions/msz.c
+++ server/src/commands/functions/msz.c
@@ -5,16 +5,22 @@
 ** msz
 */
 
+#include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 #include "commands/functions.h"
 
 void msz(list_t *args, client_t *client, server_t *server)
 {
     (void)args;
-    (void)server;
-    char *content = strdup("msz X Y\n");
+    char *content = NULL;
+    size_t width = server->params->width;
+    size_t height = server->params->height;
+    size_t len = snprintf(NULL, 0, "msz %ld %ld\n", width, height) + 1;
 
+    content = malloc(sizeof(char) * len);
     if (content == NULL)
         return;
+    sprintf(content, "msz %ld %ld\n", width, height);
     list_add(&client->output_messages, content);
 }
