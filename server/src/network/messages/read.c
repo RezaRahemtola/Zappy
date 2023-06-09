a4782e7380580cbb3a754bf7230ee3bccbafba4a
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-09 02:11:07 +0200

    feat(server): Send map size on connect

--- server/src/network/messages/read.c
+++ server/src/network/messages/read.c
@@ -14,6 +14,19 @@
 #include "network/messages.h"
 #include "commands/commands.h"
 
+static void send_welcome(client_t *client, server_t *server, size_t clientsNb)
+{
+    size_t w = server->params->width;
+    size_t h = server->params->height;
+    size_t len = snprintf(NULL, 0, "%ld\n%ld %ld\n", clientsNb, w, h) + 1;
+    char *data = malloc(sizeof(char) * len);
+
+    if (data == NULL)
+        return;
+    sprintf(data, "%ld\n%ld %ld\n", clientsNb, w, h);
+    list_add(&client->output_messages, data);
+}
+
 static void handle_team_message(client_t *client, char *msg, server_t *server)
 {
     list_t *teams = server->params->teams;
@@ -30,7 +43,7 @@ static void handle_team_message(client_t *client, char *msg, server_t *server)
         return;
     client->team = found->name;
     found->clientsNb--;
-    dprintf(client->socket->fd, "%ld\n", found->clientsNb);
+    send_welcome(client, server, found->clientsNb);
 }
 
 static void handle_message(client_t *client, char *msg, server_t *server)
