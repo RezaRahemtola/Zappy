8029a851de54fe7c12eb312498565aae7dc0351d
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-06-09 14:53:02 +0200

    feat: add request and collect to ZappyGUI

--- gui/src/ZappyGUI/ZappyGUI.cpp
+++ gui/src/ZappyGUI/ZappyGUI.cpp
@@ -18,11 +18,28 @@ void ZappyGUI::pollEvents() {
 }
 
 void ZappyGUI::run() {
+    std::vector<std::string> messageQueue;
+
     while (_window.isOpen()) {
+        std::cout << "SENDING REQUESTS :" << std::endl;
+        _client.sendRequest("msz\n");
+        _client.sendRequest("bct 0 0\n");
+        _client.sendRequest("mct\n");
+        _client.sendRequest("tna\n");
+        _client.sendRequest("ppo 0\n");
+        _client.sendRequest("plv 0\n");
+        _client.sendRequest("pin 0\n");
+        _client.sendRequest("sgt\n");
+        _client.collectMessage();
         pollEvents();
         _window.clear();
         if (_currentDisplay != "Quit")
             _currentDisplay = _displays[_currentDisplay]->run(_window);
         _window.display();
+        messageQueue = _client.getMessageQueue();
+        std::cout << "RECEIVING MESSAGES :" << std::endl;
+        for (auto &message : messageQueue) {
+            std::cout << " - recieved : " << message << std::endl;
+        }
     }
 }
