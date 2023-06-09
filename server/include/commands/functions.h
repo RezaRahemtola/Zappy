81bacd931a5ebc850889f15e2e87edde65a9ff99
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-09 23:10:14 +0200

    feat(server/commands): AI and GUI separation

--- server/include/commands/functions.h
+++ server/include/commands/functions.h
@@ -9,7 +9,8 @@
 
 #include "types.h"
 
-#define UNKNOWN_COMMAND "suc\n"
+#define UNKNOWN_COMMAND_GUI "suc\n"
+#define UNKNOWN_COMMAND_AI "ko\n"
 #define UNKNOWN_COMMAND_PARAMETER "sbp\n"
 
 void msz(list_t *args, client_t *client, server_t *server, char **result);
