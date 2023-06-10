2614ba13fe1a88bc534e19f5138e13c858082d4a
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-10 11:08:30 +0200

    feat(server/commands): Player inventory command

--- server/include/commands/functions.h
+++ server/include/commands/functions.h
@@ -19,5 +19,6 @@ void sgt(list_t *args, client_t *client, server_t *server, char **result);
 void sst(list_t *args, client_t *client, server_t *server, char **result);
 void ppo(list_t *args, client_t *client, server_t *server, char **result);
 void plv(list_t *args, client_t *client, server_t *server, char **result);
+void pin(list_t *args, client_t *client, server_t *server, char **result);
 
 void connect_nbr(list_t *args, client_t *client, server_t *serv, char **);
