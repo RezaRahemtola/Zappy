e6037c7bd9e8d89882130354d99993b954622f4f
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-10 10:56:00 +0200

    feat(server/commands): Player level command

--- server/src/commands/gui/player.c
+++ server/src/commands/gui/player.c
@@ -31,7 +31,7 @@ static player_t *get_player_by_id(list_t *clients, size_t id)
 {
     client_t *client = NULL;
 
-     for (; clients != NULL; clients = clients->next) {
+    for (; clients != NULL; clients = clients->next) {
         client = clients->data;
         if (client->player != NULL && client->player->id == id)
             return client->player;
@@ -54,6 +54,27 @@ void ppo(list_t *args, client_t *client, server_t *server, char **result)
             *result = malloc(sizeof(char) * len);
             sprintf(*result, "ppo %ld %ld %ld %ld\n",
                         pl->id, pl->x, pl->y, pl->orientation);
+            return;
+        }
+    }
+    *result = strdup(UNKNOWN_COMMAND_PARAMETER);
+}
+
+void plv(list_t *args, client_t *client, server_t *server, char **result)
+{
+    (void)client;
+    player_t *pl = NULL;
+    size_t id = get_player_id(args);
+    size_t len = 0;
+
+    if (id > 0) {
+        pl = get_player_by_id(server->clients, id);
+        if (pl != NULL) {
+            len = snprintf(NULL, 0, "ppo %ld %ld\n",
+                        pl->id, pl->level) + 1;
+            *result = malloc(sizeof(char) * len);
+            sprintf(*result, "ppo %ld %ld\n",
+                        pl->id, pl->level);
             return;
         }
     }
