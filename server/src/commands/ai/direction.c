a844423543d3bf4e06a33c7d006263493b3465f8
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-10 12:30:29 +0200

    feat(server/commands): AI forward command

--- server/src/commands/ai/direction.c
+++ server/src/commands/ai/direction.c
@@ -31,3 +31,27 @@ void left(list_t *args, client_t *client, server_t *serv, char **result)
         player->orientation = 4;
     *result = strdup(SUCCESS_COMMAND_AI);
 }
+
+void forward(list_t *args, client_t *client, server_t *serv, char **result)
+{
+    player_t *player = client->player;
+    size_t width = serv->params->width;
+    size_t height = serv->params->height;
+
+    switch (player->orientation) {
+        case 1:
+            player->y = (player->y == 0) ? height - 1 : (player->y - 1);
+            break;
+        case 2:
+            player->x = (player->x + 1 >= width) ? 0 : player->x + 1;
+            break;
+        case 3:
+            player->y = (player->y + 1 >= height) ? 0 : player->y + 1;
+            break;
+        case 4:
+            player->x = (player->x == 0) ? width - 1 : player->x - 1;
+        default:
+            (void)args;
+    }
+    *result = strdup(SUCCESS_COMMAND_AI);
+}
