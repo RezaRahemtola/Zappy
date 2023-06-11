af8f8c7979f2343b1edc43423bd2f18644d179ea
Croos3r <dorian.moy@epitech.eu>
2023-06-11 21:42:57 +0200

    style(ai): reformatted files with style issues
    Took 10 seconds

--- ai/ai_client.py
+++ ai/ai_client.py
@@ -1,13 +1,17 @@
-from client import Client
 import re
+from typing import List, Tuple
+
+from client import Client
 
 SERVER_BANNER = 'WELCOME'
 
+
 class AICommand:
-    def __init__(self, call_id: str, response_format: list[re.Pattern], time_limit: int or None) -> None:
+    def __init__(self, call_id: str, response_format: List[re.Pattern], time_limit: int or None) -> None:
         self.call_id = call_id
         self.response_format = response_format
         self.time_limit = time_limit
+
 
 COMMANDS = {
     'forward': AICommand('Forward', [r'ok'], 7),
@@ -27,8 +31,9 @@ COMMANDS = {
     'incantation': AICommand('Incantation', [r'(Elevation underway|ko)', r'Current level: [1-8]'], 300)
 }
 
+
 class AIClient(Client):
-    def __init__(self, server: tuple[str, int], team_name: str, time_unit: int = 100) -> None:
+    def __init__(self, server: Tuple[str, int], team_name: str, time_unit: int = 100) -> None:
         super().__init__(server)
         self.team_name = team_name
         self.team_available_slots = 0
@@ -38,25 +43,25 @@ class AIClient(Client):
     def start_handshake(self):
         if SERVER_BANNER not in self.receive_lines(None):
             raise RuntimeError('Can\'t find server banner.')
-        
+
         self.send_line(self.team_name)
         response = self.receive_lines(None)
 
         if len(response) != 2 or not re.match(r'\d+', response[0]):
             raise RuntimeError('Invalid server response: bad handshake response format.')
-        
+
         position_match = re.match(r'([1-9]\d*) ([1-9]\d*)', response[1])
 
         if not position_match:
             raise RuntimeError('Invalid server response: bad position format.')
-        
+
         try:
             self.team_available_slots = int(response[0])
             self.position = (int(position_match[1]), int(position_match[2]))
         except ValueError:
             raise RuntimeError('Invalid server response: non decimal numbers.')
-    
-    def execute_command(self, command: AICommand, arguments: list[str] = []) -> list[str]:
+
+    def execute_command(self, command: AICommand, arguments: List[str] = []) -> List[str]:
         self.send_line(f'{command.call_id}{" " if len(arguments) else ""}{" ".join(arguments)}')
 
         response = self.receive_lines(None)
@@ -64,13 +69,12 @@ class AIClient(Client):
         actual_lines, expected_lines = len(response), len(command.response_format)
 
         if len(response) != len(command.response_format):
-            raise RuntimeError(f'Invalid server response: to command {command.call_id}, got {actual_lines} lines but expected {expected_lines} lines')
-        
+            raise RuntimeError(
+                f'Invalid server response: to command {command.call_id}, '
+                f'got {actual_lines} lines but expected {expected_lines} lines')
+
         for index, response_line in enumerate(response):
             if not re.match(command.response_format[index], response_line):
-                raise RuntimeError(f'Invalid server response: to command {command.call_id}, got invalid line \'{response_line}\'')
-        
+                raise RuntimeError(
+                    f'Invalid server response: to command {command.call_id}, got invalid line \'{response_line}\'')
         return response
-
-
-
--- ai/client.py
+++ ai/client.py
@@ -1,9 +1,11 @@
-import socket
 import os
+import socket
 import time
+from typing import Tuple, List
+
 
 class Client:
-    def __init__(self, server: tuple[str, int]) -> None:
+    def __init__(self, server: Tuple[str, int]) -> None:
         self.server = server
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connected = False
@@ -15,17 +17,17 @@ class Client:
         self.connected = True
         try:
             self.socket.connect(self.server)
-        except:
+        except socket.error:
             return False
         return True
-    
+
     def disconnect(self) -> bool:
         if not self.connected:
             raise RuntimeError('Can\'t disconnected a not already connected client.')
         self.connected = False
         try:
             self.socket.close()
-        except:
+        except socket.error:
             return False
         return True
 
@@ -40,7 +42,7 @@ class Client:
             sent += current_sent
         self.last_send_time = time.time()
 
-    def receive_lines(self, timeout: float or None, read_size: int = 2048) -> list[str]:
+    def receive_lines(self, timeout: float or None, read_size: int = 2048) -> List[str]:
         self.socket.settimeout(timeout)
         result = self.socket.recv(read_size, os.O_NONBLOCK).decode('utf8').strip().split('\n')
         self.socket.settimeout(None)
