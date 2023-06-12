c021f608cf246bb5f3ffd3dea6fd65961be3eca3
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-06-12 16:24:16 +0200

    feat(gui): base for tile class

---
+++ gui/src/GameData/Tile/Tile.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tomi-Tom on 6/12/23.
+//
+
+#include "Tile.hpp"
---
+++ gui/src/GameData/Tile/Tile.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Tomi-Tom on 6/12/23.
+//
+
+#ifndef ZAPPY_PROJECT_TILE_HPP
+#define ZAPPY_PROJECT_TILE_HPP
+
+
+class Tile {
+
+};
+
+
+#endif //ZAPPY_PROJECT_TILE_HPP
