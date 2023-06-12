eff7ec6b2738704a5c36e2f346c62364191913a6
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-06-12 16:22:34 +0200

    feat(gui): base for game data storage

---
+++ gui/src/GameData/GameData.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Tomi-Tom on 6/12/23.
+//
+
+#ifndef ZAPPY_PROJECT_GAMEDATA_HPP
+#define ZAPPY_PROJECT_GAMEDATA_HPP
+
+
+class GameData {
+
+};
+
+
+#endif //ZAPPY_PROJECT_GAMEDATA_HPP
