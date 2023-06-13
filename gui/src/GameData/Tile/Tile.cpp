038c2647bc57cf1775c523cae6256e91ac3388f8
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-06-13 06:09:47 +0200

    fix(gui): better display the number of each ressources

--- gui/src/GameData/Tile/Tile.cpp
+++ gui/src/GameData/Tile/Tile.cpp
@@ -16,79 +16,40 @@ void Tile::display(sf::RenderWindow &window) {
     border.setOutlineThickness(1);
     border.setOutlineColor(sf::Color::Black);
     tile.setPosition(_position.x + 1, _position.y + 1);
-    tile.setFillColor(sf::Color::Green);
+    tile.setFillColor(sf::Color(16, 180, 16));
     window.draw(border);
     window.draw(tile);
     dislpayRessources(window);
 }
 
 void Tile::dislpayRessources(sf::RenderWindow &window) {
-    sf::RectangleShape food(sf::Vector2f(_size.x / 5, _size.y / 5));
-    sf::RectangleShape linemate(sf::Vector2f(_size.x / 5, _size.y / 5));
-    sf::RectangleShape deraumere(sf::Vector2f(_size.x / 5, _size.y / 5));
-    sf::RectangleShape sibur(sf::Vector2f(_size.x / 5, _size.y / 5));
-    sf::RectangleShape mendiane(sf::Vector2f(_size.x / 5, _size.y / 5));
-    sf::RectangleShape phiras(sf::Vector2f(_size.x / 5, _size.y / 5));
-    sf::RectangleShape thystame(sf::Vector2f(_size.x / 5, _size.y / 5));
+    int i = 0;
 
-    sf::Text foodText(std::to_string(_ressources[Ressource::FOOD]), _font, 20);
-    sf::Text linemateText(std::to_string(_ressources[Ressource::LINEMATE]), _font, 20);
-    sf::Text deraumereText(std::to_string(_ressources[Ressource::DERAUMERE]), _font, 20);
-    sf::Text siburText(std::to_string(_ressources[Ressource::SIBUR]), _font, 20);
-    sf::Text mendianeText(std::to_string(_ressources[Ressource::MENDIANE]), _font, 20);
-    sf::Text phirasText(std::to_string(_ressources[Ressource::PHIRAS]), _font, 20);
-    sf::Text thystameText(std::to_string(_ressources[Ressource::THYSTAME]), _font, 20);
+    for (auto &ressource : _ressources) {
+        sf::Text text;
+        std::string str = std::to_string(ressource.second);
 
-    food.setPosition(_position.x + 1, _position.y + 1);
-    food.setFillColor(sf::Color::Red);
-    foodText.setPosition(_position.x + 4, _position.y - 3);
-    foodText.setFillColor(sf::Color::Black);
-
-    linemate.setPosition(_position.x + 1, _position.y + 1 + (_size.y / 5));
-    linemate.setFillColor(sf::Color::Blue);
-    linemateText.setPosition(_position.x + 1, _position.y - 3 + _size.y / 5);
-    linemateText.setFillColor(sf::Color::Black);
-
-    deraumere.setPosition(_position.x + 1, _position.y + 1 + _size.y / 5 * 2);
-    deraumere.setFillColor(sf::Color(139, 69, 19));
-    deraumereText.setPosition(_position.x + 1, _position.y + 1 + _size.y / 5 * 2);
-    deraumereText.setFillColor(sf::Color::Black);
-
-    sibur.setPosition(_position.x + 1, _position.y + 1 + _size.y / 5 * 3);
-    sibur.setFillColor(sf::Color(255, 165, 0));
-    siburText.setPosition(_position.x + 1, _position.y + 1 + _size.y / 10 + _size.y / 5 * 3);
-    siburText.setFillColor(sf::Color::Black);
-
-    mendiane.setPosition(_position.x + 1, _position.y + 1 + _size.y / 5 * 4);
-    mendiane.setFillColor(sf::Color::Magenta);
-    mendianeText.setPosition(_position.x + 1, _position.y + 1 + _size.y / 10 + _size.y / 5 * 4);
-    mendianeText.setFillColor(sf::Color::Black);
-
-    phiras.setPosition(_position.x + 1 + _size.x / 5, _position.y + 1);
-    phiras.setFillColor(sf::Color::Cyan);
-    phirasText.setPosition(_position.x + 1 + _size.x / 10 + _size.x / 5, _position.y - 3);
-    phirasText.setFillColor(sf::Color::Black);
-
-    thystame.setPosition(_position.x + 1 + _size.x / 5, _position.y + 1 + _size.y / 5);
-    thystame.setFillColor(sf::Color::White);
-    thystameText.setPosition(_position.x + 1 + _size.x / 10 + _size.x / 5, _position.y - 3 + _size.y / 5);
-    thystameText.setFillColor(sf::Color::Black);
-
-    window.draw(food);
-    window.draw(linemate);
-    window.draw(deraumere);
-    window.draw(sibur);
-    window.draw(mendiane);
-    window.draw(phiras);
-    window.draw(thystame);
-
-    window.draw(foodText);
-    window.draw(linemateText);
-    window.draw(deraumereText);
-    window.draw(siburText);
-    window.draw(mendianeText);
-    window.draw(phirasText);
-    window.draw(thystameText);
+        text.setFont(_font);
+        text.setString(str);
+        text.setCharacterSize(20);
+        if (ressource.first == Ressource::FOOD)
+            text.setFillColor(sf::Color::Red);
+        else if (ressource.first == Ressource::LINEMATE)
+            text.setFillColor(sf::Color::Yellow);
+        else if (ressource.first == Ressource::DERAUMERE)
+            text.setFillColor(sf::Color::Blue);
+        else if (ressource.first == Ressource::SIBUR)
+            text.setFillColor(sf::Color::Magenta);
+        else if (ressource.first == Ressource::MENDIANE)
+            text.setFillColor(sf::Color::Cyan);
+        else if (ressource.first == Ressource::PHIRAS)
+            text.setFillColor(sf::Color::White);
+        else if (ressource.first == Ressource::THYSTAME)
+            text.setFillColor(sf::Color::Black);
+        text.setPosition(_position.x + 15 * (i % 5) + 2, _position.y + 20 * (i / 5) + 2);
+        window.draw(text);
+        i++;
+    }
 }
 
 void Tile::updateRessource(Ressource ressource, Operation operation, std::size_t value) {
