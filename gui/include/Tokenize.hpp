c1c561388e50b8aef77f17d70e83ebf2df1ddfe5
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-06-13 05:01:49 +0200

    feat(gui): tokenize function to split strings

---
+++ gui/include/Tokenize.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** zappy_gui
+** File description:
+** Tokenize.hpp
+*/
+
+#ifndef ZAPPY_GUI_TOKENIZE_HPP
+    #define ZAPPY_GUI_TOKENIZE_HPP
+
+void tokenize(std::string const &str, const char delim, std::vector<std::string> &out);
+
+#endif //ZAPPY_GUI_TOKENIZE_HPP
---
+++ gui/src/Tokenize.cpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** zappy_gui
+** File description:
+** Tokenize.cpp
+*/
+
+#include <iostream>
+#include <vector>
+
+void tokenize(std::string const &str, const char delim, std::vector<std::string> &out)
+{
+    size_t start;
+    size_t end = 0;
+
+    while ((start = str.find_first_not_of(delim, end)) != std::string::npos)
+    {
+        end = str.find(delim, start);
+        out.push_back(str.substr(start, end - start));
+    }
+}
