46d3c3b067b3391dd6441cdbb53c3f068a868132
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-16 01:15:50 +0200

    feat(server/events): Incantation start GUI event

--- server/include/commands/events.h
+++ server/include/commands/events.h
@@ -21,6 +21,7 @@ void emit_resource_event(enum RESOURCE_EVENT evt, const char *resource,
 void emit_egg_laying_event(size_t player_id, server_t *server);
 void emit_dead_player_event(client_t *client, server_t *server);
 void emit_eject_player_event(client_t *client, server_t *server);
+void emit_incant_start_event(player_t *pl, list_t *clients);
 void emit_incant_end_event(size_t x, size_t y, bool success, list_t *clients);
 
 // Utils
--- server/src/commands/gui/events/incantation.c
+++ server/src/commands/gui/events/incantation.c
@@ -9,7 +9,30 @@
 #include <stdio.h>
 #include <malloc.h>
 #include "types.h"
-#include "parameters.h"
+#include "utils.h"
+#include "commands/events.h"
+
+void emit_incant_start_event(player_t *pl, list_t *clients)
+{
+    size_t len = snprintf(NULL, 0, "pic %ld %ld %ld", pl->x, pl->y, pl->level);
+    char *content = malloc(sizeof(char) * (len + 1));
+    char *tmp = NULL;
+    client_t *client = NULL;
+    list_t *evt_clients = clients;
+
+    sprintf(content, "pic %ld %ld %ld", pl->x, pl->y, pl->level);
+    for (; clients != NULL; clients = clients->next) {
+        client = clients->data;
+        if (client->player != NULL && client->player->x == pl->x
+        && client->player->y == pl->y && client->player->level == pl->level) {
+            len = snprintf(NULL, 0, " %ld", client->player->id) + 1;
+            tmp = malloc(sizeof(char) * len);
+            sprintf(tmp," %ld", client->player->id);
+            content = new_strcat(content, tmp, true, true);
+        }
+    }
+    send_event(evt_clients, new_strcat(content, "\n", true, false), true);
+}
 
 void emit_incant_end_event(size_t x, size_t y, bool success, list_t *clients)
 {
