5d85ff98b67b49a3bd23934091e5786647875263
Léa GUILLEMARD <lea.guillemard@epitech.eu>
2023-06-19 14:40:49 +0200

    fix(server/map): remove debug print

--- server/src/game/map/refill_map.c
+++ server/src/game/map/refill_map.c
@@ -25,33 +25,13 @@ tile_t ***fill_empty_map(tile_t ***map, size_t width, size_t height)
     return map;
 }
 
-void print_map(tile_t ***map, size_t width, size_t height)
-{
-    printf("--------------------------------------------------\n");
-    for (size_t i = 0; i < height; i++) {
-        for (size_t j = 0; j < width; j++) {
-            printf("Map[%ld][%ld] =\n", i, j);
-            printf("Food: %ld\n", map[i][j]->food);
-            printf("Linemate: %ld\n", map[i][j]->linemate);
-            printf("Deraumere: %ld\n", map[i][j]->deraumere);
-            printf("Sibur: %ld\n", map[i][j]->sibur);
-            printf("Mendiane: %ld\n", map[i][j]->mendiane);
-            printf("Phiras: %ld\n", map[i][j]->phiras);
-            printf("Thystame: %ld\n", map[i][j]->thystame);
-        }
-    }
-    printf("--------------------------------------------------\n\n\n");
-}
-
 tile_t ***refill_map(tile_t ***map, size_t width, size_t height)
 {
     tile_t *new_resources = NULL;
     size_t x = 0;
     size_t y = 0;
 
-    print_map(map, width, height);
     map = fill_empty_map(map, width, height);
-    print_map(map, width, height);
     new_resources = spawn_resource(map, width, height);
     if (new_resources == NULL)
         return map;
