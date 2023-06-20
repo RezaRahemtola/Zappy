a29416642cd42e2feffc1f23b8ea2a57f0be4c32
pablolevy <pablo.levy@epitech.eu>
2023-06-20 16:30:29 +0200

    fix(ai): travel function for ai don't take list anymore

--- ai/travel.py
+++ ai/travel.py
@@ -2,45 +2,22 @@ from ai_client import AIClient, COMMANDS
 from typing import List, Tuple
 
 
-def move_forward(coordinates: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
-    updated_coordinates = []
-    for x, y in coordinates:
-        updated_coordinates.append((x, y - 1))
-    return updated_coordinates
-
-
-def rotate_right(coordinates: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
-    updated_coordinates = []
-    for x, y in coordinates:
-        updated_coordinates.append((y, -x))
-    return updated_coordinates
-
-
-def rotate_left(coordinates: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
-    updated_coordinates = []
-    for x, y in coordinates:
-        updated_coordinates.append((-y, x))
-    return updated_coordinates
-
-
-def travel(client: AIClient, coordinates: List[Tuple[int, int]]) -> None:
-    while coordinates:
-        x, y = coordinates[0]
-        if x == 0 and y == 0:
-            coordinates.pop(0)
-            continue
-        if y > 0:
+def travel(client: AIClient, posx: int, posy: int) -> None:
+    while True:
+        if posx == 0 and posy == 0:
+            break
+        if posy > 0:
             client.execute_command(COMMANDS['Forward'])
-            coordinates = move_forward(coordinates)
-        if y < 0:
+            posy -= 1
+        if posy < 0:
             client.execute_command(COMMANDS['Right'])
-            coordinates = rotate_right(coordinates)
+            posx, posy = posy, -posx
             client.execute_command(COMMANDS['Right'])
-            coordinates = rotate_right(coordinates)
-        if y == 0:
-            if x < 0:
+            posx, posy = posy, -posx
+        if posy == 0:
+            if posx < 0:
                 client.execute_command(COMMANDS['Right'])
-                coordinates = rotate_right(coordinates)
-            if x > 0:
+                posx, posy = -posy, posx
+            if posx > 0:
                 client.execute_command(COMMANDS['Left'])
-                coordinates = rotate_left(coordinates)
+                posx, posy = -posy, posx
