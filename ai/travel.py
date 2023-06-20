5f82fab0a67d534cdc85e42ec5f9ee524d76e760
pablolevy <pablo.levy@epitech.eu>
2023-06-20 11:15:48 +0200

    fix(ai): travel function taht actually works

--- ai/travel.py
+++ ai/travel.py
@@ -2,28 +2,30 @@ from ai_client import AIClient, COMMANDS
 from typing import List, Tuple
 
 
-def move_forward(coordinates: list[tuple[int, int]]) -> tuple[int, int]:
-    for i in range(len(coordinates)):
-        coordinates[i] = (coordinates[i][0], coordinates[i][1] + 1)
+def move_forward(coordinates: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    updated_coordinates = []
+    for x, y in coordinates:
+        updated_coordinates.append((x, y - 1))
+    return updated_coordinates
 
 
-def rotate_right(coordinates: list[tuple[int, int]]) -> list[tuple[int, int]]:
-    for i in range(len(coordinates)):
-        x, y = coordinates[i]
-        coordinates[i] = (y, -x)
-    return coordinates
+def rotate_right(coordinates: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    updated_coordinates = []
+    for x, y in coordinates:
+        updated_coordinates.append((y, -x))
+    return updated_coordinates
 
 
-def rotate_left(coordinates: list[tuple[int, int]]) -> list[tuple[int, int]]:
-    for i in range(len(coordinates)):
-        x, y = coordinates[i]
-        coordinates[i] = (-y, x)
-    return coordinates
+def rotate_left(coordinates: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    updated_coordinates = []
+    for x, y in coordinates:
+        updated_coordinates.append((-y, x))
+    return updated_coordinates
 
 
-def travel(client: AIClient, coordinates: list[tuple[int, int]]) -> None:
+def travel(client: AIClient, coordinates: List[Tuple[int, int]]) -> None:
     while coordinates:
-        x, y = coordinates[i]
+        x, y = coordinates[0]
         if x == 0 and y == 0:
             coordinates.pop(0)
             continue
@@ -36,9 +38,9 @@ def travel(client: AIClient, coordinates: list[tuple[int, int]]) -> None:
             client.execute_command(COMMANDS['Right'])
             coordinates = rotate_right(coordinates)
         if y == 0:
-            if x > 0:
+            if x < 0:
                 client.execute_command(COMMANDS['Right'])
                 coordinates = rotate_right(coordinates)
-            if x < 0:
+            if x > 0:
                 client.execute_command(COMMANDS['Left'])
                 coordinates = rotate_left(coordinates)
