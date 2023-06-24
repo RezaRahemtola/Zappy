41ac6139a423c5029ddd65fafa3f968e582d5769
Reza Rahemtola <reza.rahemtola@epitech.eu>
2023-06-24 23:35:09 +0200

    fix(server/events): tna showing only last team

--- server/src/commands/gui/tna.c
+++ server/src/commands/gui/tna.c
@@ -11,22 +11,38 @@
 #include "commands/functions.h"
 #include "parameters.h"
 
-void tna(list_t *args, client_t *client, server_t *server, char **result)
+static char *get_team_names(list_t *teams)
 {
-    (void)args;
-    (void)client;
+    char *result = NULL;
     team_t *team = NULL;
-    list_t *teams = server->params->teams;
-    size_t len = 0;
+    char *tmp = NULL;
 
     for (; teams != NULL; teams = teams->next) {
         team = teams->data;
         if (strcmp(team->name, GUI_TEAM_NAME) == 0)
             continue;
-        len = snprintf(NULL, 0, "tna %s\n", team->name) + 1;
-        *result = malloc(sizeof(char) * len);
-        if (*result == NULL)
-            continue;
-        sprintf(*result, "tna %s\n", team->name);
+        if (result == NULL) {
+            result = malloc(sizeof(char) * (strlen(team->name) + 2));
+            sprintf(result, " %s", team->name);
+        } else {
+            tmp = malloc(sizeof(char) *
+                    (snprintf(NULL, 0, "%s %s", result, team->name) + 1));
+            sprintf(tmp, "%s %s", result, team->name);
+            free(result);
+            result = tmp;
+        }
     }
+    return result;
+}
+
+void tna(list_t *args, client_t *client, server_t *server, char **result)
+{
+    (void)args;
+    (void)client;
+    char *names = get_team_names(server->params->teams);
+    size_t len = snprintf(NULL, 0, "tna%s\n", names) + 1;
+
+    *result = malloc(sizeof(char) * len);
+    sprintf(*result, "tna%s\n", names);
+    free(names);
 }
