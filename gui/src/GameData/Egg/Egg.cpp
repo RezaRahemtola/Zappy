d05aa6befa66ab5d7d6493400df22c1a9d687d75
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-06-25 00:25:55 +0200

    feat(gui): display of egg with size changing animation

--- gui/src/GameData/Egg/Egg.cpp
+++ gui/src/GameData/Egg/Egg.cpp
@@ -8,5 +8,15 @@
 #include "Egg.hpp"
 
 void Egg::display() {
+    if (_size >= 1.5)
+        _tendency = false;
+    else if (_size <= 0.5)
+        _tendency = true;
 
+    if (_tendency)
+        _size += 0.02;
+    else
+        _size -= 0.02;
+
+    DrawCube(_position, _size, _size, _size, WHITE);
 }
