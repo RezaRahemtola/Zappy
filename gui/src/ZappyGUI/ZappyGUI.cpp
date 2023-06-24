e4f61b371373738c66bbcd27a00cf4fb4e76c6b9
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-06-24 23:37:53 +0200

    feat(gui): add view changer by F/T keys to have FREE or TOP FIXED view

--- gui/src/ZappyGUI/ZappyGUI.cpp
+++ gui/src/ZappyGUI/ZappyGUI.cpp
@@ -20,7 +20,33 @@ void ZappyGUI::run() {
         DrawGrid(200, 1.0f);
         EndMode3D();
 
-        // Camera Handler
+        // Camera movement controled by keyboard relative to camera facing
+        handleCamera();
+
+        // 2D mode
+        DrawRectangle(0, 0, 110, 40, Fade(SKYBLUE, 0.5f));
+        DrawRectangleLines(0, 0, 110, 40, Fade(DARKBLUE, 0.5f));
+        DrawText("Welcome to the Zappy Of All Time", _width / 2 - 270, 10, 30, GRAY);
+
+        DrawText("Press [ESC] to exit", _width - 200, _height - 80, 20, GRAY);
+
+        DrawFPS(10, 10);
+        EndDrawing();
+    }
+}
+
+void ZappyGUI::handleCamera() {
+    // Set view by command
+    if (IsKeyDown(KEY_F)) {
+        _view = Views::FREE;
+    }
+    if (IsKeyDown(KEY_T)) {
+        _view = Views::TOP;
+    }
+
+    // Handle camera movement
+    if (_view == Views::FREE) {
+        // Handle camera rotation by mouse
         if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
         {
             if (IsCursorHidden()) {
@@ -35,24 +61,23 @@ void ZappyGUI::run() {
             UpdateCamera(&_camera, CAMERA_FIRST_PERSON);
         }
 
-        // Camera movement controled by keyboard relative to camera facing
+        // Update camera
         UpdateCameraPro(&_camera,
-                        (Vector3){
-                            (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))*0.1f - (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))*0.1f,
-                            (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))*0.1f - (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))*0.1f,
-                            (IsKeyDown(KEY_E) || IsKeyDown(KEY_SPACE))*0.1f - (IsKeyDown(KEY_Q) || IsKeyDown(KEY_LEFT_SHIFT))*0.1f
+                        (Vector3) {
+                            (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) * 0.2f -
+                            (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) * 0.2f,
+                            (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) * 0.2f -
+                            (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) * 0.2f,
+                            (IsKeyDown(KEY_E) || IsKeyDown(KEY_SPACE)) * 0.2f -
+                            (IsKeyDown(KEY_Q) || IsKeyDown(KEY_LEFT_SHIFT)) * 0.2f
                         },
-                        (Vector3){0.0f, 0.0f, 0.0f},
-                        GetMouseWheelMove()*2.0f);
-
-        // 2D mode
-        DrawRectangle(0, 0, 110, 40, Fade(SKYBLUE, 0.5f));
-        DrawRectangleLines(0, 0, 110, 40, Fade(DARKBLUE, 0.5f));
-        DrawText("Welcome to the Zappy Of All Time", _width / 2 - 270, 10, 30, GRAY);
-
-        DrawText("Press [ESC] to exit", _width - 200, _height - 80, 20, GRAY);
-
-        DrawFPS(10, 10);
-        EndDrawing();
+                        (Vector3) {0.0f, 0.0f, 0.0f},
+                        GetMouseWheelMove() * 2.0f);
+    } else {
+        _camera.position = { 45.0f, 50.0f, 21.0f };
+        _camera.target = { 45.0f, 0.0f, 20.0f };
+        _camera.up = { 0.0f, 3.0f, 0.0f };
+        _camera.fovy = 60.0f;
+        _camera.projection = CAMERA_PERSPECTIVE;
     }
 }
--- gui/src/ZappyGUI/ZappyGUI.hpp
+++ gui/src/ZappyGUI/ZappyGUI.hpp
@@ -15,17 +15,23 @@
     #include "Menu.hpp"
     #include "Playground.hpp"
 
+enum class Views {
+    FREE,
+    TOP,
+};
+
 class ZappyGUI {
     public:
         ZappyGUI(std::string machine, std::size_t port) : _width(1920), _height(1080) {
             InitWindow(_width, _height, "The ZOATIEST Project");
+            _view = Views::FREE;
             _displays["Menu"] = std::make_unique<Menu>();
             _displays["Playground"] = std::make_unique<Playground>(machine, port);
             _currentDisplay = "Playground";
 
             SetTargetFPS(120);
-            _camera.position = { -10.0f, -10.0f, -10.0f };
-            _camera.target = { 0.0f, 0.0f, 0.0f };
+            _camera.position = { 45.0f, 50.0f, 21.0f };
+            _camera.target = { 45.0f, 0.0f, 20.0f };
             _camera.up = { 0.0f, 3.0f, 0.0f };
             _camera.fovy = 60.0f;
             _camera.projection = CAMERA_PERSPECTIVE;
@@ -44,6 +50,9 @@ class ZappyGUI {
         std::string _currentDisplay;
 
         Camera3D _camera;
+        Views _view;
+
+        void handleCamera();
 };
 
 #endif /*SIMULATION_HPP_*/
